98d2d644d946c8c9c4ce8654ee211421b8464b32
EduardoRFS <theeduardorfs@gmail.com>
2020-05-11 12:38:15 -0300

    sysroot: add android.x86_64

---
+++ sysroot/android.x86_64/setup.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+set -e
+set -u
+
+NDK_ROOT=$1
+NDK_PREBUILT_BIN=$2
+
+for FILE in $NDK_BIN/x86_64-linux-android*; do
+
+FILE_ALIAS="$cur__bin/$(basename $FILE)"
+cat > "$FILE_ALIAS" <<EOF
+#!/bin/sh
+$FILE "\$@"
+EOF
+chmod +x "$FILE_ALIAS"
+
+done
+
+cat > $cur__install/toolchain.cmake <<EOF
+set(ANDROID_ABI x86_64)
+set(ANDROID_NATIVE_API_LEVEL 24) # API level
+
+include($NDK_ROOT/build/cmake/android.toolchain.cmake)
+EOF
