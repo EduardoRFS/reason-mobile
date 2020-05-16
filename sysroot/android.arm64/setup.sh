e9e2db9f88206125fc2a147aa52da6e6ff98627b
EduardoRFS <theeduardorfs@gmail.com>
2020-05-16 00:40:48 -0300

    sysroot: move android to new platform
    * use gen-tools to be consistent across platforms

---
+++ sysroot/android.arm64/setup.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+set -e
+set -u
+
+TOOLCHAIN="android.arm64"
+BASE_TRIPLE="aarch64-linux-android"
+TARGET_TRIPLE="aarch64-linux-android24"
+
+gen_tools () {
+  TOOL_NAME="$1"
+  HOST="$(which $TOOL_NAME)"
+  TARGET="$(which $TARGET_TRIPLE-$TOOL_NAME || which $BASE_TRIPLE-$TOOL_NAME)"
+
+  sysroot-gen-tools "$TOOLCHAIN" "$TARGET_TRIPLE" "$TOOL_NAME" "$TARGET" "$HOST"
+}
+
+gen_tools ar
+gen_tools as
+gen_tools clang
+gen_tools clang++
+gen_tools ld
+gen_tools nm
+gen_tools objdump
+gen_tools ranlib
+gen_tools strip
+
+cat > $cur__install/toolchain.cmake <<EOF
+set(ANDROID_ABI arm64-v8a)
+set(ANDROID_NATIVE_API_LEVEL 24) # API level
+
+include($NDK_ROOT/build/cmake/android.toolchain.cmake)
+EOF
