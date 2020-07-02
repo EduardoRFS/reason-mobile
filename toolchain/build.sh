b23e949d77f238177cfeb0fc3f141d3338f0a10a
EduardoRFS <theeduardorfs@gmail.com>
2020-07-02 03:09:10 -0300

    toolchain:deleted, it's not needed anymore

--- toolchain/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-TOOLCHAIN_USER="${TOOLCHAIN_USER:reason}"
-
-./update.sh
-docker build -f Dockerfile.base -t eduardorfs/reason-mobile:base context
-docker build --build-arg TOOLCHAIN_USER=reason -t eduardorfs/reason-mobile:latest - < Dockerfile
--- toolchain/toolchain.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-TOOLCHAIN_USER="${TOOLCHAIN_USER:reason}"
-
-mkdir -p .esy
-docker run --rm -it \
-  --mount type=bind,source="$(pwd)",target=/app \
-  eduardorfs/reason-mobile:latest su reason -l
