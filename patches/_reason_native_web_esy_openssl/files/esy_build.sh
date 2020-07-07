855bee9c7a167104e3d7f862d6e0d2af2e4cc913
EduardoRFS <theeduardorfs@gmail.com>
2020-07-07 19:00:15 -0300

    patches: esy-openssl support to ios

--- patches/_reason_native_web_esy_openssl/files/esy_build.sh
+++ patches/_reason_native_web_esy_openssl/files/esy_build.sh
@@ -3,6 +3,7 @@
 set -e
 set -u
 
+export OCAMLFIND_TOOLCHAIN=$ESY_TOOLCHAIN
 if [ "$ESY_TOOLCHAIN_SYSTEM" == "android" ]; then
   export ANDROID_NDK_HOME="$NDK_ROOT"
   export PATH="$NDK_PREBUILT/bin:$PATH"
@@ -15,8 +16,17 @@ if [ "$ESY_TOOLCHAIN_SYSTEM" == "android" ]; then
     --openssldir=$cur__install/ssl \
     android-$ESY_TOOLCHAIN_PROCESSOR \
     -D__ANDROID_API__=$ANDROID_API
+elif [ "$ESY_TOOLCHAIN_SYSTEM" == "ios" ]; then
+  export CROSS_TOP="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer"
+  export CROSS_SDK="iPhoneOS.sdk"
+  export CC="$ESY_TOOLCHAIN_CC"
+
+  ./openssl/Configure \
+    --prefix=$cur__install \
+    --openssldir=$cur__install/ssl \
+    iphoneos-cross
 else
-  exit 1;
+  ./openssl/config --prefix=$cur__install --openssldir=$cur__install/ssl --cross-compile-prefix=${ESY_TOOLCHAIN_HOST}-;
 fi
 
 make -j8
